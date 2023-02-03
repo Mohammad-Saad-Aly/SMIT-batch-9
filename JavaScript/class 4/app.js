6a417f45ffe0e48b711edd4c0611d6c9e9d47cdc
Mohammad Saad Ali <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2023-02-04 02:57:35 +0500

    arrays

---
+++ JavaScript/class 4/app.js
@@ -0,0 +1,80 @@
+// arrays 
+// var a = 'Saad'
+// var b = ' Ali'
+// var age = 23
+// var uni = 'SMiu'
+// var naam;
+// console.log(naam)
+
+// document.write(arr[0])
+// document.write(arr[0]," ",arr[1])
+
+// push last se insert
+// pop last se dlt
+// shift shru se dlt
+// unshift shru se insert
+// slice copy
+// splice beech se element ko add ya delete
+
+
+
+// arr.push('abcd','Ek new value')
+// arr.pop()
+// arr.pop()
+// document.write(arr)
+
+// var arr = []
+// arr.push('Saad')
+// arr.pop()
+// document.write(arr)
+
+// arr.splice(1,2,'Hamza')
+// document.write(arr)
+// document.write(arr2)
+// arr.unshift('kesa diya')
+// arr.shift()
+
+
+
+// var arr = ['Saad', 'Ali', 23, false]
+// var arr2 = arr.slice(0,3)
+// document.write(arr2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
